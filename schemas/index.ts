4e928489b868910b21df2736def248005ff1da02
devansh <devanshverma193@gmail.com>
2024-08-01 16:33:22 +0530

    auth-added

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,14 @@ export const LoginSchema = z.object({
     }),
 });
 
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message:"Email is required",
+    }),
+    password: z.string().min(8,{
+        message:"Minimum 8 characters required",
+    }),
+    name: z.string().min(1,{
+        message:"Name is required",
+    }),
+});
